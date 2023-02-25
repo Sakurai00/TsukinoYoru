73e54b6f24e2227b11cc6fcb9f42bb10a0e87e17
sakurai <K.Sakurai_2000@outlook.com>
2023-02-25 13:07:18 +0900

    feat: backup

---
+++ backup/backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MISSKEY_DIR=misskey
+BACKUP_DIR=backup
+DB_SERVICE=db
+USERNAME=sakurai
+
+cd "$HOME/${MISSKEY_DIR}"
+
+if [ ! -e BACKUP_DIR ]; then
+    mkdir -p ${BACKUP_DIR};
+fi
+
+DUMPFILE_NAME=misskey_bk_`date "+%Y%m%d"`.sql.gz
+
+docker compose exec ${DB_SERVICE} pg_dumpall -U ${USERNAME} | gzip -c > ${BACKUP_DIR}/${DUMPFILE_NAME}
+
+# sudo mount -t nfs 192.168.0.20:/volume1/misskey_bk /mnt/nfs
+cp ${BACKUP_DIR}/${DUMPFILE_NAME} /mnt/nfs/
